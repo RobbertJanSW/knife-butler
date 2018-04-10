4369c4769821844d19ec87a73775a87f6f29f5df
Robbert-Jan Sperna Weiland <rspernaweiland@schubergphilis.com>
2018-04-10 14:25:44 +0200

    Changed custom run command behaviour

--- lib/chef/knife/butler_run.rb
+++ lib/chef/knife/butler_run.rb
@@ -43,8 +43,7 @@ module KnifeButler
       bootstrap.config[:chef_node_name] = butler_data['server_name']
       bootstrap.config[:chef_server] = false
       bootstrap.config[:payload_folder] = butler_runner_windows_path
-      bootstrap.config[:bootstrap_run_command] = 'C:\chef\extra_files\butler_runner_windows.ps1'
-      bootstrap.config[:chef_server] = false
+      bootstrap.config[:bootstrap_run_command] = 'powershell.exe -file C:\chef\extra_files\butler_runner_windows.ps1'
       # bootstrap.config[:bootstrap_run_command] = 'get-childitem C:\chef\extra_files'
 
       puts "Starting bootstrap.."
