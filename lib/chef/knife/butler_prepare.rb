3cbf00054ec97dd5a5f7e9245758d291080ba2fe
Robbert-Jan Sperna Weiland <rspernaweiland@schubergphilis.com>
2018-09-20 21:19:57 +0200

    We dont need this anymore with sane templates

--- lib/chef/knife/butler_prepare.rb
+++ lib/chef/knife/butler_prepare.rb
@@ -119,11 +119,6 @@ module KnifeButler
       puts "Waiting for WinRM......"
       wait_for_port_open(test_config['driver']['customize']['pf_ip_address'], butler_data['port_exposed_winrm'])
       puts "WinRM available!"
-      puts "Sleeping two minutes..."
-      sleep(120)
-      puts "Rechecking.."
-      wait_for_port_open(test_config['driver']['customize']['pf_ip_address'], butler_data['port_exposed_winrm'])
-      puts "WinRM available!"
     end
 
     def config_fetch
