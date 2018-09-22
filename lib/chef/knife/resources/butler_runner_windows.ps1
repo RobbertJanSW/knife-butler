1e30cb05c762666ba6367dd5f94d2cdba7dbbcb0
Robbert-Jan Sperna Weiland <rspernaweiland@schubergphilis.com>
2018-09-22 17:58:17 +0200

    Make sure databag folders are copied to right path

--- lib/chef/knife/resources/butler_runner_windows.ps1
+++ lib/chef/knife/resources/butler_runner_windows.ps1
@@ -8,6 +8,7 @@ $repo = $test_data.repository
 mkdir -f C:\Programdata\Butler\data_bags
 mkdir -f C:\Users\ADMINI~1\AppData\Local\Temp\kitchen\cache
 copy-item -Recurse "C:\ProgramData\butler\cookbooks\$($repo_name)\test\fixtures\data_bags\*.*" "C:\ProgramData\butler\data_bags"
+copy-item -Recurse "C:\ProgramData\butler\cookbooks\$($repo_name)\test\fixtures\data_bags\*" "C:\ProgramData\butler\data_bags"
 copy-item "C:\ProgramData\butler\cookbooks\$($repo_name)\test\integration\default\encrypted_data_bag_secret" "C:\Chef"
 New-Item C:\Programdata\butler\validation_key -ItemType file
 copy-item "C:\ProgramData\butler\cookbooks\$($repo_name)\test\environments\*.*" "C:\Programdata\butler\environments"
