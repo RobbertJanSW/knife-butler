0585a926831d0978f08f63f4b22cbe68b65d10b7
Robbert-Jan Sperna Weiland <rspernaweiland@schubergphilis.com>
2018-09-22 11:44:56 +0200

    Compatibility with kitchen

--- lib/chef/knife/resources/templates/chef-solo.rb
+++ lib/chef/knife/resources/templates/chef-solo.rb
@@ -1,6 +1,6 @@
 node_name 'tsttst101'
 checksum_path 'C:\Programdata\butler\checksums'
-file_cache_path 'C:\chef\cache'
+file_cache_path 'C:\Users\ADMINI~1\AppData\Local\Temp\kitchen\cache'
 file_backup_path 'C:\Programdata\butler\backup'
 cookbook_path 'C:\Programdata\butler\cookbooks'
 data_bag_path 'C:\Programdata\butler\data_bags'
