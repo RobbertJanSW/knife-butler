ffcfd43ff13396a8423c884c9b5ea2367d74c3d4
Robbert-Jan Sperna Weiland <rspernaweiland@schubergphilis.com>
2018-09-22 11:34:56 +0200

    Keep cache path in line with default

--- lib/chef/knife/resources/butler_runner_windows.ps1
+++ lib/chef/knife/resources/butler_runner_windows.ps1
@@ -5,6 +5,7 @@ $repo = $test_data.repository
 
 # Copy data bags from repo to databags path
 mkdir -f C:\Programdata\Butler\data_bags
+mkdir -f C:\chef\cache
 copy-item -Recurse "C:\ProgramData\butler\cookbooks\$($repo_name)\test\fixtures\data_bags" "C:\ProgramData\butler\data_bags"
 copy-item "C:\ProgramData\butler\cookbooks\$($repo_name)\test\integration\default\encrypted_data_bag_secret" "C:\Chef"
 New-Item C:\Programdata\butler\validation_key -ItemType file
