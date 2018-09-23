0046d458e8aa7ae67eb63f5a5af91fbd40297310
Robbert-Jan Sperna Weiland <rspernaweiland@schubergphilis.com>
2018-09-23 19:38:48 +0200

    Catch cases in which Chef returns exit code 0 for failed run

--- lib/chef/knife/resources/butler_runner_windows.ps1
+++ lib/chef/knife/resources/butler_runner_windows.ps1
@@ -15,7 +15,12 @@ copy-item "C:\ProgramData\butler\cookbooks\$($repo_name)\test\environments\*.*" 
 
 
 c:\opscode\chef\bin\chef-client.bat -z -E $environment -c C:\ProgramData\butler\chef-solo.rb -o "$runlist" -L C:\chef\client.log
+# Chef manages to exit with return code 0, even when failing and creating a stacktrace file. So lets check for that:
+$resultcode = $LASTEXITCODE
+if (($resultcode -eq 0) -And (Test-Path C:\Users\Administrator\AppData\Local\Temp\kitchen\cache\chef-stacktrace.out)) {
+  $resultcode = 7382
+}
 # Register exit for bootstrap console cmd script
-$LASTEXITCODE | Out-File -FilePath C:\chef\ps_exitcode.txt
+$resultcode | Out-File -FilePath C:\chef\ps_exitcode.txt
 # Remove log to trigger continuation of bootstrap console script
 while (Test-Path 'C:\chef\client.log') { Remove-Item 'C:\chef\client.log' -Force -ErrorAction SilentlyContinue }
