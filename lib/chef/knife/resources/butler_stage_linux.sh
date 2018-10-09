707c4b57a0f8c82d9f335603c779d3ffb099882b
Robbert-Jan Sperna Weiland <rspernaweiland@schubergphilis.com>
2018-10-09 15:41:20 +0200

    Fix validation_key path for linux

--- lib/chef/knife/resources/butler_stage_linux.sh
+++ lib/chef/knife/resources/butler_stage_linux.sh
@@ -4,5 +4,5 @@ mkdir /tmp/butler/data_bags
 mkdir /tmp/butler/kitchen/cache
 cp -r "/tmp/butler/cookbooks/$repo/test/fixtures/data_bags/*" "/tmp/butler/data_bags/"
 cp -r "/tmp/butler/cookbooks/$repo/test/integration/default/encrypted_data_bag_secret" /var/chef
-touch C:\Programdata\butler\validation_key
+touch /tmp/butler/validation_key
 cp "/tmp/butler/cookbooks/$repo/test/environments/*" /tmp/butler/environments/
