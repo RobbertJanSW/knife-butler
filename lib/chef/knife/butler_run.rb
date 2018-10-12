775cc30a7eaf5ef2bb973a62be0bead9e5f04ba1
Robbert-Jan Sperna Weiland <rspernaweiland@schubergphilis.com>
2018-10-12 15:27:31 +0200

    This is not needed

--- lib/chef/knife/butler_run.rb
+++ lib/chef/knife/butler_run.rb
@@ -235,8 +235,6 @@ module KnifeButler
 
         # send remote
         files_send('/tmp/chef_omnibus_install.sh', '/tmp/chef_omnibus_install.sh', butler_data)
-        # remote commmand: chmod to runnable
-        command_run('sudo sh chmod 755 /tmp/chef_omnibus_install.sh', butler_data)
 
         # remote command: "sh $tmp_dir/install.sh -P chef <%= latest_current_chef_version_string %>"
         command_run('sudo sh /tmp/chef_omnibus_install.sh -P chef -v "12.21.4"', butler_data)
