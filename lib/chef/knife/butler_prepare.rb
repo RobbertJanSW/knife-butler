da292097a246058f57803b00103362a393cb8a98
Robbert-Jan Sperna Weiland <rspernaweiland@schubergphilis.com>
2018-10-12 13:28:44 +0200

    Fix typo

--- lib/chef/knife/butler_prepare.rb
+++ lib/chef/knife/butler_prepare.rb
@@ -102,7 +102,7 @@ module KnifeButler
       firewall_rule.config[:public_ip] = butler_data['test_config']['driver']['customize']['pf_ip_address']
       firewall_rule.run
 
-      berks_zip berks_thread.join.value
+      berks_zip=berks_thread.join.value
       puts berks_zip
       sleep(30)
       butler_data['berks_zip'] = berks_zip
